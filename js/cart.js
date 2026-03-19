af49db33f5150542d0f402686ad701ec0e28efd9
mazen Abu Sharha <azyra537@gmail.com>
2026-03-19 21:29:00 +0300

    Add functions to manage cart items

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,13 @@
+// line 1
+let cart = [];
 
+// line 3
+function addToCart(product) {
+  cart.push(product);
+  updateCartUI();
+}
+
+// line 8
+function removeFromCart(id) {
+  cart = cart.filter(p => p.id !== id);
+}
